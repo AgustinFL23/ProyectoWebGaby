c1042726f255f5c14ceb5f7ae7529ee43ff8e18d
Agustin <agustin@agustin>
2025-05-13 11:43:35 -0600

    cookie iniciada

--- PHP/login.php
+++ PHP/login.php
@@ -2,18 +2,18 @@
 session_start();
 include 'conexion.php';
 
-$usuario = $_POST['usuario'] ?? '';
+$correo = $_POST['correo'] ?? '';
 $contrasena = $_POST['contrasena'] ?? '';
 $tipo = $_POST['tipo'] ?? '';
 
-$sql = "SELECT * FROM usuarios WHERE usuario=? AND contrasena=? AND tipo=?";
+$sql = "SELECT * FROM usuarios WHERE correo=? AND contrasena=? AND tipo=?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $usuario, $contrasena, $tipo);
+$stmt->bind_param("sss", $correo, $contrasena, $tipo);
 $stmt->execute();
 $resultado = $stmt->get_result();
 
 if ($resultado->num_rows > 0) {
-    $_SESSION['usuario'] = $usuario;
+    $_SESSION['correo'] = $correo;
     $_SESSION['tipo'] = $tipo;
     $_SESSION['last_interaction'] = time();
 
