21320859ff3fbc1fd7b194b5867f19b6ca33b9d3
Agustin <agustin@agustin>
2025-05-13 11:43:35 -0600

    cookie iniciada

--- SQL/BaseProvisional.sql
+++ SQL/BaseProvisional.sql
@@ -3,13 +3,13 @@ USE proyecto_web;
 
 CREATE TABLE IF NOT EXISTS usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(50) NOT NULL UNIQUE,
+    correo VARCHAR(50) NOT NULL UNIQUE,
     contrasena VARCHAR(100) NOT NULL,
     tipo ENUM('alumno', 'profesor', 'administrador') NOT NULL
 );
 
 -- Ejemplo de datos
-INSERT INTO usuarios (usuario, contrasena, tipo) VALUES
-('joshua', '1234', 'alumno'),
-('maria', 'abcd', 'profesor'),
-('admin', 'adminpass', 'administrador');
+INSERT INTO usuarios (correo, contrasena, tipo) VALUES
+('joshua@gmail.com', '1234', 'alumno'),
+('maria@gmail.com', 'abcd', 'profesor'),
+('admin@gmail.com', 'adminpass', 'administrador');
