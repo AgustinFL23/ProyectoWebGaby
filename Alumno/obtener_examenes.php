a10e5aaaf0245ed900963169f57f78e63fe836b8
Agustin <agustin@agustin>
2025-05-20 15:55:05 -0600

    Reestructuracion del proyecto

---
+++ Alumno/obtener_examenes.php
@@ -0,0 +1,23 @@
+<?php
+require_once 'conexion.php'; 
+
+header('Content-Type: application/json');
+
+$sql = "SELECT * FROM examenes";
+$result = $conn->query($sql);
+
+$examenes = [];
+
+if ($result && $result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        $examenes[] = $row;
+    }
+}
+
+$response = [
+    "cantidad" => count($examenes),
+    "examenes" => $examenes
+];
+
+echo json_encode($response);
+?>
---
+++ Comun/Javascript/login.js
@@ -0,0 +1,29 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+
+	const correo = document.getElementById("correo").value;
+	const contrasena = document.getElementById("password").value;
+	const tipo = document.getElementById("tipo").value;
+
+	const datos = new URLSearchParams();
+	datos.append("correo", correo);
+	datos.append("contrasena", contrasena);
+	datos.append("tipo", tipo);
+
+	fetch("PHP/login.php", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/x-www-form-urlencoded"
+		},
+		body: datos.toString()
+	})
+	.then(res => res.json())
+	.then(data => {
+		if (data.success) {
+			window.location.href = data.redirect;
+		} else {
+			alert("Usuario o contraseña incorrectos");
+		}
+	})
+	.catch(() => alert("Error al conectar con el servidor."));
+});
