17aa54bcb9e498f746583e92d13a4284b65ac1a7
Agustin <agustin@agustin>
2025-06-18 17:42:27 -0600

    agregando

---
+++ Alumno/Imprimibles/Javascript/Imprimibles.js
@@ -0,0 +1,12 @@
+function Mostrar_Formulario() {
+    const id=obtener_Id_Desde_URL();
+    var iframe = document.getElementsByClassName("frame-formularios-alumno"); //un iframe para mostrar los formularios
+    console.log("../"+id);
+    if (iframe.length>0) {
+        iframe[0].src="../"+id;   
+    }
+    else{
+        console.log("No se encontro el iframe");
+    }
+
+}
